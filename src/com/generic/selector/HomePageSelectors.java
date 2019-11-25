3d04689a2e1102810b721b5b037441b40987b6ad
emad-tabib <37978382+emad-tabib@users.noreply.github.com>
2019-11-25 12:30:54 +0200

    iUpdated iPAd selector for search field

--- src/com/generic/selector/HomePageSelectors.java
+++ src/com/generic/selector/HomePageSelectors.java
@@ -11,7 +11,7 @@ public class HomePageSelectors
 	public static final cselector miniCartProductContainer = new cselector("mini-cart-items-container","m-mini-cart__content-products");
 	public static final cselector miniCartCheckoutBtn = new cselector("css,.mini-cart-product-panel-btn","pw-button pw--primary u-width-full");		
 	public static final cselector searchIconOpen = new cselector("css,#uNavTop >.searchboxAsButton","css,.t-header__search");
-	public static final cselector searchIconClose = new cselector("css,#search-popup > .search-popup-close-icon","css,.c-search-action__close-button");
+	public static final cselector searchIconClose = new cselector("css,#search-popup > .search-popup-close-icon","css,.left-container>.leftSideNav>button","css,.c-search-action__close-button");
 	public static final cselector searchIconField = new cselector("css,#searchBox>#headerBox","css,#search-0");
 	public static final String moduleHeroImg = "css,#homepage-head div.row div.genericESpot div.module-hero.full-width div.col-xs-12 div.wrapper a img.img.img-responsive";
 	public static final cselector carusals =new cselector( "css,.tilePanel>div>div>a>img","css,.genericESpot>div>div>div>.pw-scroller>div>.pw-scroller__content>.pw-scroller__item>article>a>div>div>div>div>div>img");
