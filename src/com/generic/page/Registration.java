3c7534a6486295939bd3f6de42112fd05f659688
tariqalrob97 <tariq.testitg@gmail.com>
2019-12-08 11:08:03 +0200

    Registration Fix

--- src/com/generic/page/Registration.java
+++ src/com/generic/page/Registration.java
@@ -10,6 +10,7 @@ import com.generic.setup.ExceptionMsg;
 import com.generic.setup.GlobalVariables;
 import com.generic.setup.LoggingMsg;
 import com.generic.setup.SelTestCase;
+import com.generic.util.RandomUtilities;
 import com.generic.util.SelectorUtil;
 import java.net.URI;
 
@@ -187,7 +188,7 @@ public class Registration extends SelTestCase {
 				typeCompany(companyName);
 
 			if (!"".equals(addressDetails.get(AddressBook.shippingAddress.keys.adddressLine)))
-				typeAddressLine1(addressDetails.get(AddressBook.shippingAddress.keys.adddressLine));
+				typeAddressLine1(RandomUtilities.getRandomName());
 
 			if (!"".equals(addressDetails.get(AddressBook.shippingAddress.keys.city)))
 				typeCity(addressDetails.get(AddressBook.shippingAddress.keys.city));
@@ -199,7 +200,7 @@ public class Registration extends SelTestCase {
 				typeZipcode(addressDetails.get(AddressBook.shippingAddress.keys.zipcode));
 
 			if (!"".equals(addressDetails.get(AddressBook.shippingAddress.keys.phone))) {
-				typePhone(addressDetails.get(AddressBook.shippingAddress.keys.phone));
+				typePhone(RandomUtilities.getRandomPhone());
 
 				clickSaveButton();
 			}
