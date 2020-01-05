cc2f3cd44b6677b9a2a43fd66ccdf3e65d6e2311
walahamad <33720328+walaahamad@users.noreply.github.com>
2020-01-05 14:04:15 +0200

    Merge General (GH & RY) Login.

--- GH/com/generic/tests/GH/login/LoginBase.java
+++ GH/com/generic/tests/GH/login/LoginBase.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -49,6 +50,9 @@ public class LoginBase extends SelTestCase {
 	public void LoginRegressionTest(String caseId, String runTest, String desc, String proprties, String email,
 			String fieldsValidation) {
 
+		String CaseDescription = MessageFormat.format(LoggingMsg.TEST_CASE_DESC, testDataSheet + "." + caseId,
+				this.getClass().getCanonicalName(), desc.replace("\n", "<br>--"));
+		initReportTime();
 		Testlogs.set(new SASLogger("Login " + getBrowserName()));
 		// Important to add this for logging/reporting
 		setTestCaseReportName("Login Case");
@@ -128,15 +132,13 @@ public class LoginBase extends SelTestCase {
 			}
 
 			sassert().assertAll();
-			Common.testPass();
+			Common.testPass(CaseDescription);
 		} catch (Throwable t) {
-			setTestCaseDescription(getTestCaseDescription());
-			Testlogs.get().debug(MessageFormat.format(LoggingMsg.DEBUGGING_TEXT, t.getMessage()));
-			t.printStackTrace();
-			String temp = getTestCaseReportName();
-			Common.testFail(t, temp);
-			ReportUtil.takeScreenShot(getDriver(), testDataSheet + "_" + caseId);
-			Assert.assertTrue(false, t.getMessage());
+			if ((getTestStatus() != null) && getTestStatus().equalsIgnoreCase("skip")) {
+				throw new SkipException("Skipping this exception");
+			} else {
+				Common.testFail(t, CaseDescription, testDataSheet + "_" + caseId);
+			}
 		} // catch
 	}// test
 }
